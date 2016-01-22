3b6ca4fbd30ea53b94056cbb2f07afd9b7848d0d
Carlos Marcano <cmarcano631@gmail.com>
2016-01-22 12:05:25 -0430

    Refactor class Delete isGlobal() Create expose()

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 'use strict'
 
 /**
- *
+ * Base Service
+ * Inherit of this class for create services
  */
 module.exports = class ProtonService {
 
   constructor(app) {
-    if (!app.services) {
-      app.services = {}
-    }
-
-    app.services[this.name] = this
     this.app = app
-
-    if (this.isGlobal()) {
-      global[this.name] = this
-    }
+    this._bindToApp()
+    this.expose()
   }
 
-  isGlobal() {
-    return true
-  }
-
+  /**
+   * @return class name
+   */
   get name() {
     return this.constructor.name
   }
+
+  /**
+   * @description Expose this service globally,
+   * Overwrite and return false for not expose the service
+   *
+   * @return true | false
+   */
+  expose() {
+    global[this.name] = this
+    return true
+  }
+
+  /**
+   * @description Bind this service to the app.services objects
+   */
+  _bindToApp() {
+    this.app.services[this.name] = this
+  }
+
 }
